f10636ea2c8b6db02960273236e2d2d0592037fc
sasha <zooblin@gmail.com>
2022-04-18 06:00:46 +0300

    concurrent processing

--- colStats/main.go
+++ colStats/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime"
+	"sync"
 )
 
 func main() {
@@ -41,24 +43,61 @@ func run(filenames []string, op string, column int, out io.Writer) error {
 
 	consolidate := make([]float64, 0)
 
-	for _, fname := range filenames {
-		f, err := os.Open(fname)
-		if err != nil {
-			return fmt.Errorf("Cannot open file: %w", err)
-		}
+	resCh := make(chan []float64)
+	errCh := make(chan error)
+	doneCh := make(chan struct{})
+	filesCh := make(chan string)
+	
+	wg := sync.WaitGroup{}
 
-		data, err := csv2float(f, column)
-		if err != nil {
-			return err
+	go func() {
+		defer close(filesCh)
+		for _, fname := range filenames {
+			filesCh <- fname
 		}
+	}()
 
-		if err := f.Close(); err != nil {
-			return err
-		}
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
 
-		consolidate = append(consolidate, data...)
+		go func() {
+			defer wg.Done()
+
+			for fname := range filesCh {
+				f, err := os.Open(fname)
+				if err != nil {
+					errCh <- fmt.Errorf("Cannot open file: %w", err)
+					return
+				}
+	
+				data, err := csv2float(f, column)
+				if err != nil {
+					errCh <- err
+				}
+	
+				if err := f.Close(); err != nil {
+					errCh <- err
+				}
+	
+				resCh <- data
+			}
+		}()
 	}
 
-	_, err := fmt.Fprintln(out, opFunc(consolidate))
-	return err
+	go func() {
+		wg.Wait()
+		close(doneCh)
+	}()
+
+	for {
+		select {
+		case err := <-errCh:
+			return err
+		case data := <-resCh:
+			consolidate = append(consolidate, data...)
+		case <-doneCh:
+			_, err := fmt.Fprintln(out, opFunc(consolidate))
+			return err
+		}
+	}
 }
