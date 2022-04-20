99bbfd9c52ad50526d8f3a4ab4a61ab961fc25da
sasha <zooblin@gmail.com>
2022-04-20 18:54:24 +0300

    timeoutstep

--- goci/main.go
+++ goci/main.go
@@ -10,7 +10,6 @@ import (
 type executer interface {
   execute() (string, error)
 }
-
 func main() {
 	proj := flag.String("p", "", "Project name")
 	flag.Parse()
@@ -26,11 +25,12 @@ func run(proj string, out io.Writer) error {
 		return fmt.Errorf("Project directory is required: %w", ErrValidation)
 	}
 	
-	pipeline := make([]executer, 3)
+	pipeline := make([]executer, 4)
 
 	pipeline[0] = newStep("go build", "go", "Go Build: SUCCESS", proj, []string{"build", ".", "errors"})
 	pipeline[1] = newStep("go test", "go", "Go Test: SUCCESS", proj, []string{"test", "-v"})
   pipeline[2] = newExceptionStep("go fmt", "gofmt", "GoFmt: SUCCESS", proj, []string{"-l", "."})
+  // pipeline[3] = newTimeoutStep("git push", "git", "Go push: SUCCESS", proj, []string{"push", "origin", "master"}, time.Second * 10)
 
 	for _, s := range pipeline {
 		msg, err := s.execute()
