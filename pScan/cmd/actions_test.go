60f5f4e4c38b730c9c47255553eb03b95c9b6281
sasha <zooblin@gmail.com>
2022-04-26 06:43:13 +0300

    add ports scan cmd

--- pScan/cmd/actions_test.go
+++ pScan/cmd/actions_test.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -118,6 +120,10 @@ func TestIntegration(t *testing.T) {
 	expectedOut += fmt.Sprintf("Deleted host: %s\n", delHost)
 	expectedOut += strings.Join(hostsEnd, "\n")
 	expectedOut += fmt.Sprintln()
+	for _, v := range hostsEnd {
+		expectedOut += fmt.Sprintf("%s: Host not found\n", v)
+		expectedOut += fmt.Sprintln()
+	}
 
 	if err := addAction(&out, tf, hosts); err != nil {
 		t.Fatalf("Expecter no error, got %q\n", err)
@@ -132,6 +138,64 @@ func TestIntegration(t *testing.T) {
 	}
 
 	if err := listAction(&out, tf, nil); err != nil {
+		t.Fatalf("Expecter no error, got %q\n", err)
+	}
+
+	if err := scanAction(&out, tf, nil); err != nil {
+		t.Fatalf("Expecter no error, got %q\n", err)
+	}
+
+	if out.String() != expectedOut {
+		t.Errorf("Expected output %q, got %q\n", expectedOut, out.String())
+	}
+}
+
+func TestScanAction(t *testing.T) {
+	hosts := []string{
+		"localhost",
+		"unknowhostoutthere",
+	}
+
+	tf, cleanup := setup(t, hosts, true)
+	defer cleanup()
+
+	ports := []int{}
+
+	for i := 0; i < 2; i++ {
+		ln, err := net.Listen("tcp", net.JoinHostPort("localhost", "0"))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer ln.Close()
+
+		_, portStr, err := net.SplitHostPort(ln.Addr().String())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		port, err := strconv.Atoi(portStr)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		ports = append(ports, port)
+
+		if i == 1 {
+			ln.Close()
+		}
+	}
+
+	expectedOut := fmt.Sprintln("localhost:")
+	expectedOut += fmt.Sprintf("\t%d: open\n", ports[0])
+	expectedOut += fmt.Sprintf("\t%d: closed\n", ports[1])
+	expectedOut += fmt.Sprintln()
+	expectedOut += fmt.Sprintln("unknowhostoutthere: Host not found")
+	expectedOut += fmt.Sprintln()
+
+	var out bytes.Buffer
+
+	if err := scanAction(&out, tf, ports); err != nil {
 		t.Fatalf("Expecter no error, got %q\n", err)
 	}
 
---
+++ pScan/cmd/scan.go
@@ -0,0 +1,95 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/sashaaKr/golang_command_line/pScan/scan"
+	"github.com/spf13/cobra"
+)
+
+// scanCmd represents the scan command
+var scanCmd = &cobra.Command{
+	Use:   "scan",
+	Short: "Run a port scan on the hosts",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFiles, err := cmd.Flags().GetString("hosts-file")
+		if err != nil {
+			return err
+		}
+
+		ports, err := cmd.Flags().GetIntSlice("ports")
+		if err != nil {
+			return nil
+		}
+
+		return scanAction(os.Stdout, hostsFiles, ports)
+	},
+}
+
+func scanAction(out io.Writer, hostsFile string, ports []int) error {
+	hl := &scan.HostsList{}
+
+	if err := hl.Load(hostsFile); err != nil {
+		return err
+	}
+
+	results := scan.Run(hl, ports)
+
+	return printResult(out, results)
+}
+
+func printResult(out io.Writer, results []scan.Results) error {
+	message := ""
+
+	for _, r := range results {
+		message += fmt.Sprintf("%s:", r.Host)
+
+		if r.NotFound {
+			message += fmt.Sprintf(" Host not found\n\n")
+			continue
+		}
+
+		message += fmt.Sprintln()
+
+		for _, p := range r.PortStates {
+			message += fmt.Sprintf("\t%d: %s\n", p.Port, p.Open)
+		}
+
+		message += fmt.Sprintln()
+	}
+
+	_, err := fmt.Fprint(out, message)
+	return err
+}
+
+func init() {
+	rootCmd.AddCommand(scanCmd)
+	scanCmd.Flags().IntSliceP("ports", "p", []int{22, 80, 443}, "Ports to scan")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// scanCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// scanCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
