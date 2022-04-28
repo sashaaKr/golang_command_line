e8a5ed0b155e3b26aab31c6a4869e8979a78fb9f
sasha <zooblin@gmail.com>
2022-04-28 06:19:17 +0300

    http api

---
+++ todoApi/go.mod
@@ -0,0 +1,7 @@
+module github.com/sashaaKr/golang_command_line/todoApi
+
+go 1.17
+
+require github.com/sashaaKr/golang_command_line/todo v0.0.0
+
+replace github.com/sashaaKr/golang_command_line/todo => ../todo
---
+++ todoApi/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	host := flag.String("h", "localhost", "hostname")
+	port := flag.Int("p", 8080, "port")
+	todoFile := flag.String("f", "todoServer.json", "todo JSON file")
+	flag.Parse()
+
+	s := &http.Server{
+		Addr:         fmt.Sprintf("%s:%d", *host, *port),
+		Handler:      newMux(*todoFile),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
